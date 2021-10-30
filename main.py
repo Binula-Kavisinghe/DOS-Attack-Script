f28d64bf0ea9fd03b4a5d56ed33bf37ed9f48ab6
Binula Kavisinghe <81571678+Binula-Kavisinghe@users.noreply.github.com>
2021-10-30 17:58:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print("\n\n")
 print("         +-------------------------------------+")
 print("         |          nov, 18th, 2020            |")
 print("         |  This is a simple DOS attack script |")
-print("         |   Github: https://github.com/d4az   |")
+print("         |   Github: https://github.com/d4az   |")    #Startup Message
 print("         |      Author: Dasith Vidanage        |")
 print("         |            Version: 0.1             |")
 print("         +---------------------------d4az------+ ")
